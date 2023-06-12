9f54ee15ed89b5fdaf8df900f13abd280f1391fa
FelipeSnJose <snfelipe96@gmail.com>
2023-06-12 16:07:58 -0400

    set App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-const App = ()=> {
+import React from 'react';
+import Registro from './components/Registro';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './main.css';
 
+const App = () => {
   return (
     <>
-
+      <Registro />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
